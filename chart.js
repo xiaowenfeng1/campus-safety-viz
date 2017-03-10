afa0da86194692ca0d616ba58711eba65537603f
Xiaowen Feng <xfeng1@uw.edu>
2017-03-09 22:15:45 -0800

    finished part2

--- chart.js
+++ chart.js
@@ -1,77 +1,86 @@
 function makeBarchart() {
+  var color = d3.scaleOrdinal()
+    .range(["#bdd7e7","#6baed6","#2171b5"]);
+  var cDataset;
+  var keys;
+  var cMargin = {top: 20, right: 20, bottom: 30, left: 50},
+  cWidth = 550 - cMargin.left - cMargin.right,
+  cHeight = 400 - cMargin.top - cMargin.bottom;
 
-var cDataset;
-var keys;
-var cMargin = {top: 20, right: 20, bottom: 30, left: 50},
-    cWidth = 950 - cMargin.left - cMargin.right,
-    cHeight = 600 - cMargin.top - cMargin.bottom;
+  // create svg
+  var svg = d3.select('#area').append('svg')
+  .attr('width', cWidth + cMargin.left + cMargin.right)
+  .attr('height', cHeight + cMargin.top + cMargin.bottom+45)
+  .attr('class', 'text-center')
+  g = svg.append('g')
+  .attr('transform', 'translate(' + cMargin.left + ',' + cMargin.top + ')');
 
-// create svg
-var svg = d3.select('#area').append('svg')
-    .attr('width', cWidth + cMargin.left + cMargin.right)
-    .attr('height', cHeight + cMargin.top + cMargin.bottom)
-    .attr('class', 'text-center')
-    g = svg.append('g')
-    .attr('transform', 'translate(' + cMargin.left + ',' + cMargin.top + ')');
-
-var color = d3.scaleOrdinal(d3.schemeSet2);
-var divTooltip = d3.select("body")
+  var divTooltip = d3.select("body")
   .append("div")
   .attr("class", "toolTip")
 
-var x0 = d3.scaleBand()
-    .rangeRound([0, cWidth])
-    .paddingInner(0.1);
+  var x0 = d3.scaleBand()
+  .rangeRound([0, cWidth])
+  .paddingInner(0.1);
 
-var x1 = d3.scaleBand()
+  var x1 = d3.scaleBand()
   .padding(0.01);
 
-var y = d3.scaleLinear()
+  var y = d3.scaleLinear()
   .rangeRound([cHeight, 0]);
 
-var xAxis = d3.axisBottom()
+  var xAxis = d3.axisBottom()
   .scale(x0);
 
-var yAxis = d3.axisLeft()
+  var yAxis = d3.axisLeft()
   .scale(y);
 
-// read in data
-d3.csv('arrest.csv', function(d, i, columns) {
-  for (var i = 2, n = columns.length; i< n; i++) d[columns[i]] = +d[columns[i]];
-  return d;
-}, function(error, data) {
-  if (error) throw error;
+  // read in data
+  d3.csv('./data/arrest.csv', function(d, i, columns) {
+    for (var i = 2, n = columns.length; i< n; i++) d[columns[i]] = +d[columns[i]];
+    return d;
+  }, function(error, data) {
+    if (error) throw error;
     cDataset = data;
     //initial viz
-    drawViz(cDataset, false);
-});
+    drawChart(cDataset, false);
+  });
 
-d3.selectAll('select')
+  d3.selectAll('select')
   .on('change', function() {
     var selected = this.value;
     var file;
     if (selected == 'criminal') {
-      file = 'criminal.csv';
-    } else if (selected == 'discipline') {
-      file = 'discipline.csv';
+      file = './data/criminal.csv';
+      d3.select('.text-muted').style("visibility", "visible");
+    } else if (selected == 'VAWA') {
+      file = './data/VAWA.csv';
+      d3.select('.text-muted').style("visibility", "visible");
     } else if (selected == 'hatecrimes') {
-      file = 'hatecrimes.csv';
+      file = './data/hatecrimes.csv';
+      d3.select('.text-muted').style("visibility", "hidden");
     } else {
-      file = 'arrest.csv'; //on start
+      file = './data/arrest.csv'; //on start
+      d3.select('.text-muted').style("visibility", "hidden");
     }
     d3.csv(file, function(d, i, columns) {
       for (var i = 2, n = columns.length; i< n; i++) d[columns[i]] = +d[columns[i]];
       return d;
     }, function(error, data) {
       if (error) throw error;
-      drawViz(data, true);
+      drawChart(data, true);
     })
   });
 
-  function drawViz(data, update) {
+  function drawChart(data, update) {
     // get year headers
     keys = data.columns.slice(2);
     drawLegend();
+
+    var t = d3.transition()
+    .duration(750)
+    //.delay(100)
+    .ease(d3.easeLinear)
 
     // set x and y scale domains
     x0.domain(data.map(function(d){return d.subtype; }))
@@ -79,101 +88,118 @@ d3.selectAll('select')
     y.domain([0, d3.max(data, function(d) {
       return d3.max(keys, function(key) { return d[key]; }); })]).nice();
 
-    // create a group of bars for each subtype
-    var chart = g.selectAll(".group")
+      // create a group of bars for each subtype
+      var chart = g.selectAll(".group")
       .data(data)
-    chart.exit().remove();
-    // enter
-    var newChart = chart.enter().append("g")
+      chart.exit().transition().duration(100).remove();
+      // enter
+      var newChart = chart.enter().append("g")
       .attr("class", "group")
 
-    chart = chart.merge(newChart)
+      chart = chart.merge(newChart)
       .attr("transform", function(d) { return "translate(" + x0(d.subtype) + ",0)"; })
 
-    // create a rect for each data value
-    var rect = chart.selectAll("rect")
-      .data(function(d) { return keys.map(function(key)
-        {return {key: key, value: d[key], subtype: d.subtype}; }); })
+      // create a rect for each data value
+      var rect = chart.selectAll("rect")
+      .data(function(d) {
+        return keys.map(function(key)
+        {return {key: key, value: d[key], subtype: d.subtype}; });
+      })
+
       rect.exit().remove();
-    // update rects
-    var newRect = rect.enter().append("rect")
-      .attr("fill", function(d) { return color(d.key); });
+      // update rects
+      var newRect = rect.enter().append("rect")
+      .attr("fill", function(d) { return color(d.key); })
+      .attr("class", function(d) { return "y" + d.key});
 
-    rect = rect.merge(newRect)
-    rect.transition()
-      .duration(1000)
-      .attr("x", function(d) { return x1(d.key); })
-      .attr("y", function(d) { return y(d.value); })
-      .attr("width", x1.bandwidth())
-      .attr("height", function(d) { return cHeight - y(d.value); })
+      rect = rect.merge(newRect)
+        .attr("x", function(d) { return x1(d.key); })
+        .attr("width", x1.bandwidth())
+        .attr("y", function(d) {return cHeight})
+        .attr("height",0)
+      rect.transition(t)
+        .attr("height", function(d) { return cHeight - y(d.value); })
+        .attr("y", function(d) { return y(d.value); })
 
-    rect.on("mousemove", function(d){
-      divTooltip.style("left", d3.event.pageX+10+"px");
-      divTooltip.style("top", d3.event.pageY-25+"px");
-      divTooltip.style("display", "inline-block");
-      divTooltip.style("text-align", "left");
-      var x = d3.event.pageX, y = d3.event.pageY
-      var elements = document.querySelectorAll(':hover');
-      l = elements.length
-      l = l-1
-      elementData = elements[l].__data__
+      rect.on("mousemove", function(d){
+        divTooltip.style("left", d3.event.pageX+10+"px");
+        divTooltip.style("top", d3.event.pageY-25+"px");
+        divTooltip.style("display", "inline-block");
+        divTooltip.style("text-align", "left");
+        var x = d3.event.pageX, y = d3.event.pageY
+        var elements = document.querySelectorAll(':hover');
+        l = elements.length
+        l = l-1
+        elementData = elements[l].__data__
 
-      divTooltip.html(d.subtype + "<br>Year: " + d.key + "<br>Occurrence: " +elementData.value);
-    });
-    rect.on("mouseout", function(d){
-      divTooltip.style("display", "none");
-    });
+        divTooltip.html(d.subtype + "<br>Year: " + d.key + "<br>Occurrence: " +elementData.value);
+      });
+      rect.on("mouseout", function(d){
+        divTooltip.style("display", "none");
+      });
 
-    if (update) {
-      rect.transition()
-        .duration(1000)
+      if (update) {
+        rect.transition(t)
         .attr("height", function(d) { return cHeight - y(d.value); })
         .attr("x", function(d) { return x1(d.key); })
         .attr("y", function(d) { return y(d.value); })
         .attr("width", x1.bandwidth())
 
-      g.select(".xaxis")
-         .transition()
-         .duration(1000)
-         .call(xAxis);
+        g.select(".xaxis")
+        //.transition(t)
+        //.duration(1000)
+        .call(xAxis);
 
-      g.select(".yaxis")
-         .transition()
-         .duration(1000)
-         .call(yAxis);
+        g.select('.xaxis')
+        .selectAll(".tick text")
+        .attr("transform", "rotate(-25)")
+        .style("text-anchor", "end");
 
-    } else {
-      g.append('g')
-          .attr('class', 'xaxis')
-          .attr('transform', 'translate(0,' + cHeight + ')')
-          .call(xAxis);
+        g.select(".yaxis")
+        .transition(t)
+        //.duration(1000)
+        .call(yAxis);
 
-      g.append('g')
-          .attr('class', 'yaxis')
-          .call(yAxis);
+      } else {
+        g.append('g')
+        .attr('class', 'xaxis')
+        .attr('transform', 'translate(0,' + cHeight + ')')
+        .call(xAxis);
+
+        g.select('.xaxis')
+        .selectAll(".tick text")
+        .attr("transform", "rotate(-25)")
+        .style("text-anchor", "end");
+
+        g.append('g')
+        .attr('class', 'yaxis')
+        .call(yAxis);
+      }
     }
+
+    function drawLegend() {
+      var legend = svg.selectAll(".legend")
+          .data(keys.slice())
+          .enter().append("g")
+          .attr("class", "legend")
+          .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; })
+          .on("click", function (d) {
+            drawViz(d, false)
+          })
+
+      legend.append("rect")
+        .attr("x", cWidth)
+        .attr("width", 18)
+        .attr("height", 18)
+        .style("fill", color);
+
+      legend.append("text")
+        .attr("x", cWidth - 10)
+        .attr("y", 9)
+        .attr("dy", ".32em")
+        .style("text-anchor", "end")
+        .text(function(d) { return d; });
+    }
+
   }
-
-  function drawLegend() {
-    var legend = svg.selectAll(".legend")
-        .data(keys.slice())
-        .enter().append("g")
-        .attr("class", "legend")
-        .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
-
-    legend.append("rect")
-      .attr("x", cWidth)
-      .attr("width", 18)
-      .attr("height", 18)
-      .style("fill", color);
-
-    legend.append("text")
-      .attr("x", cWidth - 10)
-      .attr("y", 9)
-      .attr("dy", ".35em")
-      .style("text-anchor", "end")
-      .text(function(d) { return d; });
-  }
-}
-
-makeBarchart();
+  makeBarchart();
--- sunburst.js
+++ sunburst.js
@@ -1,119 +1,245 @@
-function makeSunburst() {
 var dataset;
 // Dimensions of sunburst.
-var width = 750;
-var height = 500;
+var width = 550;
+var height = 400;
 var radius = Math.min(width, height) / 2;
-
+var colors =  d3.scaleOrdinal(d3.schemeSet2);
+// Breadcrumb dimensions: width, height, spacing, width of tip/tail.
+var b = {
+  w: 120, h: 30, s: 3, t: 10
+};
+var node; //save root for tweening
 var x = d3.scaleLinear()
-    .range([0, 2 * Math.PI]);
+.range([0, 2 * Math.PI]);
 var y = d3.scaleSqrt()
-    .range([0, radius]);
-var color = d3.scaleOrdinal(d3.schemeSet2);
+.range([0, radius]);
 var partition = d3.partition()
 
 var svg = d3.select("#sunburst").append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .append("g")
-    .attr("transform", "translate(" + width / 2 + "," + (height / 2) + ")");
+.attr("width", width)
+.attr("height", height)
+.append("g")
+.attr("transform", "translate(" + width / 2 + "," + (height / 2) + ")");
 
 var arc = d3.arc()
-    .startAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x0))); })
-    .endAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x1))); })
-    .innerRadius(function(d) { return Math.max(0, y(d.y0)); })
-    .outerRadius(function(d) { return Math.max(0, y(d.y1)); });
+.startAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x0))); })
+.endAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x1))); })
+.innerRadius(function(d) { return Math.max(0, y(d.y0)); })
+.outerRadius(function(d) { return Math.max(0, y(d.y1)); });
 
 // create hierarchial data format
 var nest = d3.nest()
-    .key(function(d) { return d.type; })
-    .key(function(d) { return d.subtype; })
-    //.key(function(d){ return d.location;})
+//.key(function(d) {return d.crime;})
+.key(function(d) { return d.type; })
+.key(function(d) { return d.subtype; })
 
-d3.csv("total.csv", function(error,data) {
+d3.csv("./data/total.csv", function(error,data) {
   if (error) throw error;
   dataset = data;
-  drawViz(dataset);
- });
+  drawViz(2012, true);
+});
 
- function drawViz(data) {
-   //bouding circle underneath sunburst
-   svg.append("svg:circle")
-      .attr("r", radius)
-      .style("opacity", 0);
+function drawViz(year, first) {
 
-   var root = d3.hierarchy({values: nest.entries(data)}, function(d) { return d.values; })
-       .sort(function(a, b) { return b.value - a.value; });
-       root.sum(function(d) { return d.y2012; });
-   var nodes = partition(root).descendants();
+  initializeBreadcrumbTrail();
+  //get all years
+  keys = dataset.columns.slice(3);
 
-   svg.selectAll("path")
-     .data(nodes)
-     .enter().append("path")
-     .attr("pointer-events", "all")
-     .attr("display", function(d) { return d.depth ? null : "none"; })
-     .attr("d", arc)
-     .attr("fill-rule", "evenodd")
-     .style("fill", function(d) { return color((d.children ? d : d.parent).data.key); })
-     .style("opacity", 1)
-     .on("mouseover", mouseover);
+  //put data into hierarchy and sum up dat based on year
+  var root = d3.hierarchy({values: nest.entries(dataset)}, function(d) { return d.values; })
+  node = root; //save the original root for tween
 
+  //determine size of slices
+  if (year == "2012") {
+    root.sum(function(d) { return d.y2012; });
+  } else if (year == "2013") {
+    root.sum(function(d) { return d.y2013; });
+  } else {
+    root.sum(function(d) { return d.y2014; });
+  }
 
-  //mouseleave interaction
-  svg.on("mouseleave", mouseleave);
- }
+  var nodes = partition(root).descendants();
 
- // Fade all but the current sequence, and show it in the breadcrumb trail.
-function mouseover(d) {
-  console.log(d);
-  var count = (d.data.type ? d.data.type + " " : "")+
-  (d.parent.data.key ? d.parent.data.key + " ": "")
-  + (d.data.key ? d.data.key +" " : "")
-  +(d.data.location ? d.data.location + " ": "")
-  +d.value;
-  // var countString = count;
-  // if (count < 10) {
-  //   countString = "< 10";
-  // }
-  //console.log(count);
-  d3.select("#explanation")
-      .style("visibility", "visible");
-  d3.select("#countString")
-      .html(count);
+  if (first) {
+    var path= svg.selectAll("path")
+    .data(nodes)
+    .enter().append("path")
+    .attr("pointer-events", "all")
+    // don't show root
+    .attr("display", function(d) { return d.depth ? null : "none"; })
+    .attr("fill-rule", "evenodd")
+    .style("fill", function(d) {
+      return colors(getColor(d))})
+      .style("opacity", 1)
 
-  var sequenceArray = d.ancestors().reverse();
-  sequenceArray.shift(); // remove root node from the array
-  // updateBreadcrumbs(sequenceArray, percentageString);
+    } else {
+      svg.selectAll("path").data(nodes)
+    }
+    svg.selectAll("path")
+    .on("mouseover", mouseover)
+    .transition().duration(1000)
+    .attrTween("d", arcTweenData);
 
-  // Fade all the segments.
-  d3.selectAll("path")
-      .style("opacity", 0.3);
+    svg.on("mouseleave", mouseleave);
 
-  // Then highlight only those that are an ancestor of the current segment.
-  svg.selectAll("path")
-      .filter(function(node) {
-                return (sequenceArray.indexOf(node) >= 0);
-              })
-      .style("opacity", 1);
-}
+    // Fade all but the current sequence, and show it in the breadcrumb trail.
+    function mouseover(d) {
+      if (d) {
+        var count = d.value;
 
-function mouseleave(d) {
+        var sequenceArray = d.ancestors().reverse();
+        sequenceArray.shift(); // remove root node from the array
+        updateBreadcrumbs(sequenceArray, count);
+        // Fade all the segments.
+        d3.selectAll("path")
+        .style("opacity", 0.3);
 
-  // Deactivate all segments during transition.
-  d3.selectAll("path").on("mouseover", null);
+        // Then highlight only those that are an ancestor of the current segment.
+        svg.selectAll("path")
+        .filter(function(node) {
+          return (sequenceArray.indexOf(node) >= 0);
+        })
+        .style("opacity", 1);
+      }
+    }
 
-  // Transition each segment to full opacity and then reactivate it.
-  d3.selectAll("path")
+    function mouseleave(d) {
+      d3.select("#trail")
+      .style("visibility", "hidden");
+      // Deactivate all segments during transition.
+      d3.selectAll("path").on("mouseover", null);
+
+      // Transition each segment to full opacity and then reactivate it.
+      d3.selectAll("path")
       .transition()
-      .duration(1000)
+      .duration(500)
       .style("opacity", 1)
       .on("end", function() {
-              d3.select(this).on("mouseover", mouseover);});
+        d3.select(this).on("mouseover", mouseover);});
+      }
 
-  d3.select("#explanation")
-      .style("visibility", "hidden");
-}
+      // When switching data: interpolate the arcs in data space.
+      function arcTweenData(a, i) {
+        // (a.x0s ? a.x0s : 0) -- grab the prev saved x0 or set to 0 (for 1st time through)
+        // avoids the stash() and allows the sunburst to grow into being
+        var oi = d3.interpolate({ x0: (a.x0s ? a.x0s : 0), x1: (a.x1s ? a.x1s : 0) }, a);
+        function tween(t) {
+          var b = oi(t);
+          a.x0s = b.x0;
+          a.x1s = b.x1;
+          return arc(b);
+        }
+        if (i == 0) {
+          // If on the first arc, adjust the x domain to match the root node
+          // at the current zoom level.
+          var xd = d3.interpolate(x.domain(), [node.x0, node.x1]);
+          return function (t) {
+            x.domain(xd(t));
+            return tween(t);
+          };
+        } else {
+          return tween;
+        }
+      }
+      // color for slices
+      function getColor(d) {
+        var key;
+        if (d.parent) {
+          if (d.children) {
+            if (d.height == 2) {
+              console.log(d)
+              key = d.data.key
+            } else {
+              key = d.parent.data.key;
+            }
+          } else {
+            key = d.parent.parent.data.key;
+          }
+        }
+        return key;
+      }
 
+      function initializeBreadcrumbTrail() {
+        // Add the svg area.
+        var trail = d3.select("#sequence").append("svg:svg")
+        .attr("width", width)
+        .attr("height", 50)
+        .attr("id", "trail");
+        // Add the label at the end, for the percentage.
+        trail.append("svg:text")
+        .attr("id", "endlabel")
+        .style("fill", "#000");
+      }
 
-}
-makeSunburst();
+      // Generate a string that describes the points of a breadcrumb polygon.
+      function breadcrumbPoints(d, i) {
+        var points = [];
+        points.push("0,0");
+        points.push(b.w + ",0");
+        points.push(b.w + b.t + "," + (b.h / 2));
+        points.push(b.w + "," + b.h);
+        points.push("0," + b.h);
+        if (i > 0) { // Leftmost breadcrumb; don't include 6th vertex.
+        points.push(b.t + "," + (b.h / 2));
+      }
+      return points.join(" ");
+    }
+
+    // Update the breadcrumb trail to show the current sequence and percentage.
+    function updateBreadcrumbs(nodeArray, count) {
+
+      // Data join; key function combines name and depth (= position in sequence).
+      var trail = d3.select("#trail")
+      .selectAll("g")
+      .data(nodeArray, function(d) { return getText(d) + d.depth; });
+
+      // Remove exiting nodes.
+      trail.exit().remove();
+
+      // Add breadcrumb and label for entering nodes.
+      var entering = trail.enter().append("svg:g");
+
+      entering.append("svg:polygon")
+      .attr("points", breadcrumbPoints)
+      .style("fill", function(d) { return colors(getColor(d)); });
+
+      entering.append("svg:text")
+      .attr("x", (b.w + b.t) / 2)
+      .attr("y", b.h / 2)
+      .attr("dy", "0.35em")
+      .attr("text-anchor", "middle")
+      .style("font-size", "0.8em")
+      .text(function(d) { return getText(d); })
+
+      // Merge enter and update selections; set position for all nodes.
+      entering.merge(trail).attr("transform", function(d, i) {
+        return "translate(" + i * (b.w + b.s) + ", 0)";
+      });
+
+      // Now move and update the percentage at the end.
+      d3.select("#trail").select("#endlabel")
+      .attr("x", (nodeArray.length + 0.15) * (b.w + b.s))
+      .attr("y", b.h / 2)
+      .attr("dy", "0.35em")
+      .attr("text-anchor", "start")
+      .style("font-weight", "500")
+      .text(count +" occurrences");
+
+      // Make the breadcrumb trail visible, if it's hidden.
+      d3.select("#trail")
+      .style("visibility", "");
+
+      function getText(d) {
+        if(d.height == 0) {
+          return d.data.location;
+        } else {
+          return d.data.key;
+        }
+      }
+    }
+  }
+  // Respond to radio click.
+  d3.selectAll('.year').on("click", function change() {
+    var year = this.value;
+    drawViz(year,false);
+  });
